68160f553ae4066f6c521f1f34035e9e6a35eaad
Dan Perkins <dan@uxcreative.co>
2022-08-06 13:15:41 +0100

    Env Test II

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -28,7 +28,6 @@
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline dark:text-gray-500">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline dark:text-gray-500">Log in</a>
-
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline dark:text-gray-500">Register</a>
                         @endif
